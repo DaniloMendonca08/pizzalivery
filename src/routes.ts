71c409c904a25c803deead31b3cb4546608ee719
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-10 20:58:56 -0300

    adicionado a rota da pagina onde tera a escolha de 2 sabores

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ export const routes = {
   home: "/",
   pizzaSize: "/pedido/escolha-o-tamanho",
   pizzaFlavour: "/pedido/escolha-o-sabor",
+  pizza2Flavours: "/pedido/escolha-2-sabores",
   summary: "/pedido/resumo",
   checkout: "/checkout",
   login: "/login",
