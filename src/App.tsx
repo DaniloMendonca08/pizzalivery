d65f602c997763501d2591ca65017052b12004c6
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-13 21:59:03 -0300

    Adição das novas páginas por meio do Route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Summary from "./pages/summary/Summary"
 import Checkout from "./pages/checkout/Checkout"
 import Login from "./pages/login/Login"
 import { routes } from "./routes"
+import Twoflavours from "./pages/TwoFlavours/TwoFlavours"
+import SummaryTwoFlavours from "./pages/summaryPizzaTwoFlavours/summaryPizzaTwoFlavours"
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
         <Route path={routes.home} element={<Home />} />
         <Route path={routes.pizzaSize} element={<Sizes />} />
         <Route path={routes.pizzaFlavour} element={<Flavours />} />
+        <Route path={routes.pizza2Flavours} element={<Twoflavours />} />
         <Route path={routes.summary} element={<Summary />} />
+        <Route path={routes.summary2Flavours} element={<SummaryTwoFlavours />} />
         <Route element={<PrivateRoutes />}>
           <Route path={routes.checkout} element={<Checkout />} />
         </Route>
