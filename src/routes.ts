2ca60180c4eb2b21ecbcb252fd313c23c4acf4e0
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-13 22:07:24 -0300

    Criação da nova rota para o resumo do pedido de 2 sabores

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ export const routes = {
   pizzaFlavour: "/pedido/escolha-o-sabor",
   pizza2Flavours: "/pedido/escolha-2-sabores",
   summary: "/pedido/resumo",
+  summary2Flavours: "/pedido/resumo-2-sabores",
   checkout: "/checkout",
   login: "/login",
 }
