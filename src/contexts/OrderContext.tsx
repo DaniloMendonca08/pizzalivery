fdb88828a8a6e0ac1ab1aacb03f472f6833f94df
Danilo Mendonça <danilomendonca08@gmail.com>
2023-10-13 21:59:39 -0300

    criação de mais useStates para ser utilizado entre as páginas

--- src/contexts/OrderContext.tsx
+++ src/contexts/OrderContext.tsx
@@ -23,10 +23,15 @@ type PizzaFlavourType = {
 type PizzaOrderType = {
   item: {
     name: string
+    name2: string
     image: string
+    image2: string
     size: string
+    size2: string
     slices: number
+    slices2: number
     value: number
+    value2: number
   }
   total: number
 }
@@ -36,8 +41,11 @@ type OrderContextProps = {
   setPizzaSize: React.Dispatch<React.SetStateAction<PizzaSizeType>>
   pizzaFlavour: PizzaFlavourType
   setPizzaFlavour: React.Dispatch<React.SetStateAction<PizzaFlavourType>>
+  pizzaSecondFlavour: PizzaFlavourType
+  setPizzaSecondFlavour: React.Dispatch<React.SetStateAction<PizzaFlavourType>>
   pizzaOrder: PizzaOrderType
   setPizzaOrder: React.Dispatch<React.SetStateAction<PizzaOrderType>>
+
 }
 
 const OrderContext = createContext<OrderContextProps>({})
@@ -45,7 +53,9 @@ const OrderContext = createContext<OrderContextProps>({})
 const OrderContextProvider = ({ children }) => {
   const [pizzaSize, setPizzaSize] = useState()
   const [pizzaFlavour, setPizzaFlavour] = useState()
+  const [pizzaSecondFlavour, setPizzaSecondFlavour] = useState()
   const [pizzaOrder, setPizzaOrder] = useState()
+  const [flavourId, setFlavourId] = useState("")
 
   return (
     <OrderContext.Provider
@@ -54,8 +64,12 @@ const OrderContextProvider = ({ children }) => {
         setPizzaSize,
         pizzaFlavour,
         setPizzaFlavour,
+        pizzaSecondFlavour,
+        setPizzaSecondFlavour,
         pizzaOrder,
         setPizzaOrder,
+        flavourId,
+        setFlavourId
       }}
     >
       {children}
